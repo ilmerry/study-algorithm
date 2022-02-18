8f16756d12369dd6cb85446f2ef3b1974c6bfdd0
ilmerry <milkiss0414@naver.com>
2022-02-18 21:37:58 +0900

    ch05

---
+++ python/5-11.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 이동 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if graph[nx][ny] == 1:
+                
