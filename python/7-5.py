c7d18a426d57ee38be8cbb0baa841b52d8eb9474
ilmerry <milkiss0414@naver.com>
2022-02-28 16:01:00 +0900

    ch07_parametric_search

---
+++ python/7-5.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+n = int(input())
+array = list(map(int, input().split()))
+m = int(input())
+items = list(map(int, input().split()))
+
+array.sort()
+for item in items:
+    result = binary_search(array, item, 0, len(array) - 1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+print()
