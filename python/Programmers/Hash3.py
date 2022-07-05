8617b3e5b38024bf68c89854b764561246bdf8c3
Eunhyung Choi <milkiss0414@naver.com>
2022-07-05 11:07:30 +0900

    Programmers: Hash#3

--- python/Programmers/Hash3.py
+++ python/Programmers/Hash3.py
@@ -1,6 +1,6 @@
 def solution(clothes):
     answer = 0
-    answer += len(clothes)
+    
     # 옷 종류를 key, 개수를 value로 하는 딕셔너리
     dic = {}
     for item in clothes:
@@ -9,4 +9,11 @@ def solution(clothes):
         else:
             dic[item[1]] = 1
     
+    for i in dic:
+        if answer == 0:
+            answer = dic[i] + 1
+        else:
+            answer *= (dic[i] + 1)
+        
+    answer -= 1
     return answer
