ccca328135028a90ea84a4f6f2ea376ec6fa4d29
Eunhyung Choi <milkiss0414@naver.com>
2022-08-05 17:09:22 +0900

    Programmers: Heap#2

--- python/Programmers/Heap2.py
+++ python/Programmers/Heap2.py
@@ -1,31 +1,35 @@
 import heapq
+from collections import deque
 
 def solution(jobs):
-    answer = 0
     jobs.sort()
+    tasks = deque(jobs)
     heap = []
         
     time = 0
     total = 0
-    for req, pro in jobs:
-        # 소요시간, 요청시간
-        while req >= time:
-            if len(heap) == 0:
-                time += pro
+    task = tasks.popleft()
+    heapq.heappush(heap, [task[1], task[0]])
+    for req, pro in tasks:
+        print(req, pro)
+        while req > time:
+            if len(heap) > 0:
+                p, r = heapq.heappop(heap)
+                time = max(time + p, r + p)
+                total += (time - r)
+            else:
+                if req > time:
+                    heapq.heappush(heap, [pro, req])
+                    break
+                time = max(time + pro, req + pro)
                 total += (time - req)
-                break
-            pro, req = heapq.heappop(heap)
-            time += pro
-            total += (time - req)
         else:
-            # 이미 요청시간을 지났을 경우 소요시간 기준으로 정렬
             heapq.heappush(heap, [pro, req])
     
     while heap:
         pro, req = heapq.heappop(heap)
-        time += pro
+        time = max(time + pro, req + pro)
         total += (time - req)
-            
-    answer = total / len(jobs)
+        print(total)
     
-    return answer
+    return total // len(jobs)
