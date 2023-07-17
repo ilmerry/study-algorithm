8d0b375c0368387132513709890de9ad7f69653d
Eunhyung Choi <milkiss0414@naver.com>
2023-07-17 14:33:36 +0900

    프로그래머스Lv2: 최솟값 만들기

---
+++ Javascript/프로그래머스/Lv2/최솟값만들기.js
@@ -0,0 +1,5 @@
+function solution(A, B) {
+  const sortedA = [...A].sort((a, b) => a - b);
+  const sortedB = [...B].sort((a, b) => b - a);
+  return sortedA.reduce((acc, cur, idx) => acc + cur * sortedB[idx], 0);
+}
